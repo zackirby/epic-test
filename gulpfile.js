a63e6505e55f0c1d1651fe1f5af2739d6fa42aad
Kirby, Zack <zack.kirby@metlife.com>
2016-05-10 16:31:05 -0400

    slider, footer, about us, scrolling

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var useref = require('gulp-useref'); // file concatenation
 var uglify = require('gulp-uglify'); // minify js
 var cssnano = require('gulp-cssnano'); // minify css
 var gulpIf = require('gulp-if'); // conditionally run a task
-var imagemin = require('gulp-imagemin'); // minify images
+//var imagemin = require('gulp-imagemin'); // minify images
 var cache = require('gulp-cache'); // cache proxy
 var del = require('del'); // delete files/folders using globs - needed to delete 'dist' directory before build
 var runSequence = require('run-sequence'); // run series of dependent gulp tasks in order
@@ -73,8 +73,14 @@ gulp.task('browserSync', function(){
 	});
 });
 
+//move js files
+gulp.task('js', function(){
+	return gulp.src('app/js/**/*')
+		.pipe(gulp.dest('build/js'))
+});
+
 // concatenate html partials; concatenate and minify css and js partials
-gulp.task('useref', function(){
+gulp.task('useref', ['js'], function(){
 	return gulp.src('build/*.html')
 		.pipe(useref())
 		.pipe(gulpIf('*.js', uglify()))
@@ -87,10 +93,7 @@ gulp.task('useref', function(){
 
 // minify images
 gulp.task('images', function(){
-	return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-		.pipe(cache(imagemin({
-			interlaced: true
-		})))
+	return gulp.src('app/images/**/*')
 		.pipe(gulp.dest('build/images'))
 });
 
@@ -106,7 +109,7 @@ gulp.task('clean:build', function(){
 });
 
 // watch for reload
-gulp.task('default', ['compass','slim','images','useref','browserSync'], function(){
+gulp.task('default', ['compass','slim','js','images','useref','browserSync'], function(){
 	gulp.watch('app/**/*.slim',['slim','useref']);
 	gulp.watch('app/scss/**/*.scss',['compass']);
 	gulp.watch('app/js/*.js',['useref']);
@@ -115,7 +118,7 @@ gulp.task('default', ['compass','slim','images','useref','browserSync'], functio
 
 // build files
 gulp.task('build', function(callback){
-	runSequence('clean:build', ['compass','slim', 'images', 'fonts'], 'useref',
+	runSequence('clean:build', ['compass','slim', 'js', 'images', 'fonts'], 'useref',
 		callback
 	)
 });
